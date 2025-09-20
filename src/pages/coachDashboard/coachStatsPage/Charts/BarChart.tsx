4721aa62641127b420ea0d732ea00f552b4a010b
Matimu Khosa <2616924@students.wits.ac.za>
2025-09-20 18:50:40 +0200

    <fix> fixed some build issues

--- src/pages/coachDashboard/coachStatsPage/Charts/BarChart.tsx
+++ src/pages/coachDashboard/coachStatsPage/Charts/BarChart.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import '../MyTeamTab.css';
 import { Bar } from "react-chartjs-2";
-import { Chart as ChartJS, defaults } from "chart.js/auto";
+//import { Chart as ChartJS, defaults } from "chart.js/auto";
 
 interface Props {
   label: string[];
